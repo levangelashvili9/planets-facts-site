0492b12544571fce56c2f47703af042ebeb29f24
Levan Gelashvili <levan.gelashvili9@gmail.com>
2022-11-09 01:40:38 +0400

    fixed basename

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/planets-facts-site">
       <App />
     </BrowserRouter>
   </React.StrictMode>
